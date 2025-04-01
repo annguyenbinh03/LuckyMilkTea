53dde45ecc5f3c4c71fcc806a116bc07607ec8fd
annguyenbinh03 <phaolobinhan@gmail.com>
2025-04-01 11:23:13 +0700

    feat: create manager Layout

---
+++ PRN222.MilkTeaShop.Manager/Pages/Manager/Dashboard/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN222.MilkTeaShop.Manager.Pages.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
